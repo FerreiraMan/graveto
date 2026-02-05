fad6ddfc927cf10334db9f215266aa87f7f592e8
pedrosferreira <ferreirapedro.sjm@gmail.com>
2026-02-05 18:34:32 +0000

    create category entity

---
+++ src/main/java/me/ferreira/graveto/moneytracker/categories/Category.java
@@ -0,0 +1,62 @@
+package me.ferreira.graveto.moneytracker.categories;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.DynamicUpdate;
+
+@Getter
+@Setter
+@DynamicUpdate
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "categories")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "categories_id_seq")
+    @SequenceGenerator(name = "categories_id_seq", sequenceName = "categories_id_seq", allocationSize = 1)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private UUID sid;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(name = "display_name")
+    private String displayName;
+
+    @Column(name = "user_sid")
+    private UUID userSid;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent")
+    private List<Category> children = new ArrayList<>();
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+}
