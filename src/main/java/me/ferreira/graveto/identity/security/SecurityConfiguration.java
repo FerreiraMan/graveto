67818e85321fd13c93d31062fb58280433b8c8c0
ferreiraMan <ferreirapedro.sjm@gmail.com>
2026-08-04 11:17:36 +0100

    feat(identity): add cors handling

--- src/main/java/me/ferreira/graveto/identity/security/SecurityConfiguration.java
+++ src/main/java/me/ferreira/graveto/identity/security/SecurityConfiguration.java
@@ -1,9 +1,14 @@
 package me.ferreira.graveto.identity.security;
 
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
 import me.ferreira.graveto.common.logging.MdcLoggingFilter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -13,10 +18,16 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+
+  @Value("${http.cors.allowed-origins}")
+  private List<String> allowedOrigins;
 
   private final JwtAuthenticationFilter jwtAuthFilter;
   private final ExceptionHandlerFilter exceptionHandlerFilter;
@@ -31,6 +42,7 @@ public class SecurityConfiguration {
   public SecurityFilterChain filterChain(final HttpSecurity http) throws Exception {
 
     http
+        .cors(Customizer.withDefaults())
         .csrf(AbstractHttpConfigurer::disable)
         .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .addFilterBefore(exceptionHandlerFilter, LogoutFilter.class)
@@ -41,6 +53,19 @@ public class SecurityConfiguration {
             .anyRequest().authenticated()
         );
     return http.build();
+  }
+
+  @Bean
+  public CorsConfigurationSource corsConfigurationSource() {
+
+    final CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOrigins(allowedOrigins);
+    configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+    configuration.setAllowedHeaders(List.of("Content-Type", "Authorization"));
+    configuration.setMaxAge(Duration.ofMinutes(10));
+    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
   }
 
   @Bean
